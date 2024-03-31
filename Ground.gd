93f02adccd6d9f95ab4f7333a0315bdc4386b1ab
superlou <lousimons@gmail.com>
2024-03-31 19:09:59 -0400

    Clean up old ground

--- Ground.gd
+++ Ground.gd
@@ -5,7 +5,9 @@ var ground_velocity = 0
 @export var trainset: Trainset
 
 var tile_scene = preload("res://ground_tile.tscn")
-const TILE_WIDTH = 20.0
+const TILE_WIDTH := 20.0
+const LOOK_AHEAD := 100.0
+const LOOK_BEHIND := 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -35,9 +37,8 @@ func plant_tiles():
 	var leftmost_tile = get_child(0)
 	var rightmost_tile = get_child(-1)
 
-	print("leftmost:", leftmost_tile.position.x)
-
-	while leftmost_tile.position.x > train_min_x:
+	# Add more tiles ahead
+	while leftmost_tile.position.x > (train_min_x - LOOK_BEHIND):
 		var tile = tile_scene.instantiate()
 		tile.position.x = leftmost_tile.position.x - TILE_WIDTH
 		print(tile.position.x)
@@ -45,10 +46,16 @@ func plant_tiles():
 		move_child(tile, 0)
 		leftmost_tile = tile
 
-	while rightmost_tile.position.x < train_max_x:
+	# Add more tiles behind
+	while rightmost_tile.position.x < (train_max_x + LOOK_AHEAD):
 		var tile = tile_scene.instantiate()
 		tile.position.x = rightmost_tile.position.x + TILE_WIDTH
 		add_child(tile)
 		rightmost_tile = tile
 
-	# TODO Remove old tiles
+	# Remove old tiles behind
+	# Don't remove "old" tiles ahead so that players can't regenerate tiles
+	# that already existed.
+	if leftmost_tile.position.x < (train_min_x - LOOK_BEHIND):
+		remove_child(leftmost_tile)
+		leftmost_tile.queue_free()
