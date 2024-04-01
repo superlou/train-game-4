7a7fc5e92c6505b2f9e86c0b5576c7a59ade4de6
superlou <lousimons@gmail.com>
2024-03-31 20:12:13 -0400

    Switched to first person view

--- player.gd
+++ player.gd
@@ -1,59 +1,91 @@
-extends RigidBody3D
+extends CharacterBody3D
 
-var walk_speed := 1.0
-var sprint_speed := 3.0
-var jump_strength := 200.0
-var is_jumping := false
-var jump_cooldown := 0.1
-var jump_cooldown_count := 0.0
-var jump_velocity := Vector3.ZERO
+@export var walk_speed := 1.0 		# m/s
+@export var sprint_speed := 2.0 	# m/s
+@export var acceleration := 10.0 	# m/s^2
+@export var jump_height := 1.0 		# m
 
-# Called when the node enters the scene tree for the first time.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var look_sensitivity: float = ProjectSettings.get_setting("player/look_sensitivity")
+
+var move_dir: Vector2	# Input direction for movement
+var look_dir: Vector2	# Input direction for look/aim
+
+var walk_vel: Vector3
+var grav_vel: Vector3
+var jump_vel: Vector3
+
+var mouse_captured := false
+var jumping := false
+
+
 func _ready():
-	pass # Replace with function body.
+	capture_mouse()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if mouse_captured:
+			_rotate_camera(event)
+
+	if Input.is_action_just_pressed("jump"):
+		jumping = true
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		if mouse_captured:
+			release_mouse()
+		else:
+			capture_mouse()
+
+
+func capture_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	mouse_captured = true
+
+
+func release_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	mouse_captured = false
 
 
 func _physics_process(delta):
-	var velocity := Vector3.ZERO
+	var speed = sprint_speed if Input.is_action_pressed("sprint") else walk_speed
 
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-	
-	if Input.is_action_pressed("right"):
-		velocity.x += 1
-	
-	if Input.is_action_pressed("up"):
-		velocity.z -= 1
-	
-	if Input.is_action_pressed("down"):
-		velocity.z += 1
-	
-	velocity = velocity.normalized()
-	
-	if Input.is_action_pressed("sprint"):
-		velocity *= sprint_speed
+	velocity = _walk(delta, speed) +_gravity(delta) + _jump(delta)
+	move_and_slide()
+
+
+func _walk(delta: float, speed: float) -> Vector3:
+	move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var forward: Vector3 = %Camera.global_transform.basis * Vector3(move_dir.x, 0, move_dir.y)
+	var walk_dir := Vector3(forward.x, 0, forward.z).normalized()
+	walk_vel = walk_vel.move_toward(walk_dir * speed * move_dir.length(), acceleration * delta)
+	return walk_vel
+
+
+func _gravity(delta: float) -> Vector3:
+	if is_on_floor():
+		grav_vel = Vector3.ZERO
 	else:
-		velocity *= walk_speed
-
-	if is_jumping:
-		jump_cooldown_count -= jump_cooldown * delta
-		if is_on_floor() and jump_cooldown_count <= 0.0:
-			is_jumping = false
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		apply_force(jump_strength * Vector3.UP)
-		is_jumping = true
-		jump_cooldown_count = jump_cooldown
-		jump_velocity = velocity
-
-	if is_jumping:
-		move_and_collide(jump_velocity * delta)
-	else:
-		move_and_collide(velocity * delta)
+		grav_vel = grav_vel.move_toward(Vector3(0, velocity.y - gravity, 0), gravity * delta)
+	
+	return grav_vel
 
 
-func is_on_floor():
-	if $FloorCast.is_colliding():
-		return true
-	else:
-		return false
+func _jump(delta: float) -> Vector3:
+	if jumping:
+		if is_on_floor():
+			jump_vel = Vector3(0, sqrt(4 * jump_height * gravity), 0)
+		
+		jumping = false
+		return jump_vel
+	
+	jump_vel = Vector3.ZERO if is_on_floor() else jump_vel.move_toward(Vector3.ZERO, gravity * delta)
+	return jump_vel
+
+
+func _rotate_camera(event: InputEventMouseMotion) -> void:
+	print(look_sensitivity)
+	rotate_y(event.relative.x * -look_sensitivity)
+	%Camera.rotate_x(event.relative.y * -look_sensitivity)
+	%Camera.rotation.x = clamp(%Camera.rotation.x, -PI/2, PI/2)
