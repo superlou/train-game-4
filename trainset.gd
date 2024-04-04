30fe10ce89027e8f0d4e2935d94648acb3186dd1
superlou <lousimons@gmail.com>
2024-04-03 21:22:57 -0400

    Added braking

--- trainset.gd
+++ trainset.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 @export var head_velocity := 0.0
 @export var acceleration := 5.0
+@export var breaking := 15.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,7 +16,18 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("accelerate_left"):
 		head_velocity -= acceleration * delta
-	
+
+	if Input.is_action_pressed("apply_break"):
+		if head_velocity > 0:
+			if head_velocity - breaking * delta <= 0:
+				head_velocity = 0
+			else:
+				head_velocity -= breaking * delta
+		if head_velocity < 0:
+			if head_velocity + breaking * delta >= 0:
+				head_velocity = 0
+			else:
+				head_velocity += breaking * delta
 
 func x_bounds() -> Array[float]:
 	var x: Array[float] = [0.0]
