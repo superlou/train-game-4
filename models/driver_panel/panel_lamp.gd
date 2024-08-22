32fd15154b952034c751088e0bce868f7dc2255f
superlou <lousimons@gmail.com>
2024-08-21 20:43:16 -0400

    Made lamps fade in/out

--- models/driver_panel/panel_lamp.gd
+++ models/driver_panel/panel_lamp.gd
@@ -7,18 +7,25 @@ extends Node3D
 
 @onready var lamp_material:StandardMaterial3D = $Lamp.get_surface_override_material(0)
 
+var brightness = 0.0
 
 func _ready():
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	if active:
-		lamp_material.albedo_color = color
-		lamp_material.emission = color		
-		lamp_material.emission_enabled = true
-	else:
-		lamp_material.albedo_color = color.darkened(0.7)
-		lamp_material.emission = Color.BLACK
-		lamp_material.emission_enabled = false
+func _process(delta):
+	if active and (brightness < 1.0):
+		brightness += 20.0 * delta
+		brightness = clampf(brightness, 0.0, 1.0)
+	elif not active and (brightness > 0.0):
+		brightness -= 5.0 * delta
+		brightness = clampf(brightness, 0.0, 1.0)
+	
+	lamp_material.emission = color
+	lamp_material.emission_enabled = brightness > 0.1
+	lamp_material.albedo_color = color.darkened(
+		remap(brightness, 0., 1., 0.7, 0.0)
+	)
+
+	lamp_material.emission_energy_multiplier = brightness
