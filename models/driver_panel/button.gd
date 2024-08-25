3f18d517c7c902f98c98b25c5621cb8ab2b84a2c
superlou <lousimons@gmail.com>
2024-08-25 11:04:08 -0400

    Added labels to buttons

--- models/driver_panel/button.gd
+++ models/driver_panel/button.gd
@@ -1,15 +1,28 @@
+@tool
 extends Node3D
 
 
 signal pressed
 @export var guard_color := Color(0.255, 0.255, 0.255)
 @export var cap_color := Color.WHITE
+@export var label := "" :
+	get:
+		return label
+	set(value):
+		label = value
+		_update_label()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var guard_material:StandardMaterial3D = $Guard.mesh.surface_get_material(0).duplicate()
 	guard_material.albedo_color = guard_color
 	$Guard.mesh.surface_set_material(0, guard_material)
+	$Cap/Label.text = label
+
+
+func _update_label():
+	$Cap/Label.text = label
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
